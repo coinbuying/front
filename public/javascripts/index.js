812d50085c2289b41e185c43c24d517a1f0b8635
choisungwoo <sqlstyle@naver.com>
2022-05-05 22:35:30 +0900

    Init directory or css

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -7,20 +7,20 @@ const initView = () => {
     //로딩 class 모두 제거.
     removeLoadingItem();
 
-    document.getElementById("sign_up").addEventListener("click", function() {
+    //로그인 버튼 이벤트 
+    document.getElementById("sign_in").addEventListener("click", function() {
         setLoginModal();
     })
+
+    //회원가입 버튼 이벤트
+    document.getElementById("sign_up").addEventListener("click", function() {
+        //setLoginModal();
+    })
+
 
 }
 
 
-const eventHanlder = () => {
-
-    document.getElementById("sign_up").addEventListener("click", function() {
-        setLoginModal();
-    })
-    
-}
 
 
 
